09315d54eef9234eab12a7761223dfc466e0a4de
Aswinee <aswineepratihari010@gmail.com>
2022-06-23 20:21:17 +0530

    the initial commit

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CategoryList from '../components/CategoryList'
+import TopHeadlines from '../components/TopHeadlines'
+const Hero = () => {
+  return (
+ <>
+ <TopHeadlines/>
+ <CategoryList/>
+ </>
+  )
+}
+
+export default Hero
