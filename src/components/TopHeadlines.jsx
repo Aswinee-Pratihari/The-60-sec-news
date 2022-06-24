de6e6ca43c57a25e5223dee9352764de014d86b2
Aswinee <aswineepratihari010@gmail.com>
2022-06-24 18:29:42 +0530

    added the category and country list

--- src/components/TopHeadlines.jsx
+++ src/components/TopHeadlines.jsx
@@ -7,7 +7,7 @@ const TopHeadlines = () => {
     <section class="text-gray-600 body-font">
 <div class="container mx-auto flex px-5  items-center justify-center flex-col">
 <h1 class="sm:text-5xl text-3xl py-10 font-medium title-font mb-4 text-gray-900">TOP NEWS OF THE DAY</h1>
-  <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={"https://dummyimage.com/720x600"}/>
+  <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={"https://images.moneycontrol.com/static-mcnews/2017/03/accenture_720-770x433.jpg"}/>
   <div class="text-center lg:w-2/3 w-full">
     <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
     <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CategoryList from '../components/CategoryList'
+import CountryList from '../components/CountryList'
 import TopHeadlines from '../components/TopHeadlines'
 const Hero = () => {
   return (
  <>
  <TopHeadlines/>
+ <CountryList/>
  <CategoryList/>
  </>
   )
